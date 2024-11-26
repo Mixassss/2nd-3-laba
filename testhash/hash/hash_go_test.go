584b74e12f4ccff31045ceb401ea1dd1f285f5c4
Mixassss <bigboommaximandsim@gmail.com>
2024-11-27 01:20:32 +0700

    rename and change hash on go

--- testhash/hash/hash_go_test.go
+++ testhash/hash/hash_go_test.go
@@ -1,7 +1,6 @@
 package hash
 
 import (
-	"fmt"
 	"testing"
 )
 
@@ -35,7 +34,6 @@ func TestHashTable(t *testing.T) {
 	if removed := ht.Remove("key1"); !removed {
 		t.Error("Expected key1 to be removed")
 	}
-
 	if size := ht.Size(); size != 1 {
 		t.Errorf("Expected size 1 after removing key1, got %d", size)
 	}
@@ -50,40 +48,42 @@ func TestHashTable(t *testing.T) {
 		t.Errorf("Expected value2, got %s (found: %v)", value, found)
 	}
 
-	// Тест: Удаление несуществующего ключа
-	if removed := ht.Remove("key3"); removed {
-		t.Error("Expected key3 to be not removed")
-	}
-
 	// Тест: Вставка дубликатов
 	ht.Insert("key2", "newValue2")
 	if value, found := ht.Get("key2"); !found || value != "newValue2" {
 		t.Errorf("Expected newValue2, got %s (found: %v)", value, found)
 	}
 
-	// Тест: Проверка на пустую хеш-таблицу
-	ht.Remove("key2") // Удалим ключ перед завершением
-	if size := ht.Size(); size != 0 {
-		t.Errorf("Expected size 0 after removing key2, got %d", size)
+	// Тест: Вставка коллизий
+	ht.Insert("key3", "value3")
+	ht.Insert("key13", "value3") // Столкновение предполагаемое (зависит от хеш-функции)
+	if value, found := ht.Get("key3"); !found || value != "value3" {
+		t.Errorf("Expected value3 for key3, got %s (found: %v)", value, found)
 	}
-
-	// Тест: Попытка вставить nil ключ
-	defer func() {
-		if r := recover(); r == nil {
-			t.Errorf("Expected panic when inserting nil key")
-		}
-	}()
-	ht.Insert("", "valueNil") // Ожидаем, что вставка nil не пройдет
+	if value, found := ht.Get("key13"); !found || value != "value3" {
+		t.Errorf("Expected value3 for key13, got %s (found: %v)", value, found)
+	}
 
 	// Тест: Получение значения для несуществующего ключа
 	if value, found := ht.Get("nonExistent"); found || value != "" {
 		t.Error("Expected nonExistent to be not found")
 	}
-}
 
-func BenchmarkHashTable(b *testing.B) {
-	ht := NewHashTable()
-	for i := 0; i < b.N; i++ {
-		ht.Insert(fmt.Sprintf("key%d", i), fmt.Sprintf("value%d", i))
+	// Тест: Удаление несуществующего ключа
+	if removed := ht.Remove("key3"); !removed {
+		t.Error("Expected key3 to be removed")
+	}
+
+	// Тест: Проверка на вставку значений в пустой хэш-таблицу
+	ht = NewHashTable()
+	ht.Insert("key1", "value1")
+	if value, found := ht.Get("key1"); !found || value != "value1" {
+		t.Errorf("Expected value1 after inserting into an empty hashtable, got %s", value)
+	}
+
+	// Тест: Пустая хэш-таблица
+	ht.Remove("key2") // Убираем ключ перед завершением
+	if size := ht.Size(); size != 1 {
+		t.Errorf("Expected size 1 after removing key2, got %d", size)
 	}
 }
